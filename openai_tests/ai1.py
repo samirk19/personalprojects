860febd3929dd9221d3324c7e94f3e4de4bf7863
samirk19 <samir.khattak19@gmail.com>
2024-05-06 18:08:49 -0400

    test

--- openai_tests/ai1.py
+++ openai_tests/ai1.py
@@ -1,4 +1,31 @@
-import openai
-completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
-    {"role": "user", "content": "Hello world"}])
-print(completion.choices[0].message.content)
+import pathlib
+import textwrap
+import google.generativeai as genai
+import os
+#from IPython.display import display
+from IPython.display import Markdown
+
+key = os.getenv('GOOGLE_API_KEY')
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+
+
+if __name__ == "__main__":
+    genai.configure(api_key=key)
+    
+    """for x in genai.list_models():
+        if 'generateContent' in x.supported_generation_methods:
+            print(x.name)"""
+
+    model1 = genai.GenerativeModel('gemini-pro')
+    question = input("What do you want to ask Gemini Pro? ")
+    response = model1.generate_content(question, stream=False)
+
+    print(response.text)
+    
+    """for chunk in response:
+        print(chunk.text)
+        print("_"*80)"""
